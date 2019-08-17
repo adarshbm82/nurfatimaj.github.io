77182be286a305281a84969610019033d0490a63
Nurfatima Jandarova <nurfatima.jandarova@eui.eu>
2019-08-18 00:31:12 +0600

    rebuild site 18 Aug 19 00:31

--- js/run_pandoc.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#get_pdf').on('click', function() {
-		$.ajax({
-			url : '/layouts/partials/run_pandoc.php'
-		}).done(function(data) {
-			console.log(data);
-		});
-	});
-});
