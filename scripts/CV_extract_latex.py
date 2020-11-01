7bdebbf4fbfd83af15dc4f4b5cf3917d59fcec32
nurfatimaj <nurfatima.jandarova@gmail.com>
2020-11-01 13:23:23 +0100

    Added python script to public

---
+++ scripts/CV_extract_latex.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 14:39:42 2019
+
+@author: labuv
+"""
+
+from bs4 import BeautifulSoup
+
+input = "print/index.html"
+
+# Extract relevant part of the page
+soup = BeautifulSoup(open(input), 'html.parser')
+CV_tex = soup.find('code').text
+
+# Translate html escape to latex escape
+escape_characters = {'&percnt'  : '\%',
+                     '&amp;'    :'\&',
+                     '&gt;'     : '>',
+                     '&lt;'     : '<',
+                     '[:tab:]'  : '&'}
+CV_tex_clean = CV_tex
+for htmlesc, latexesc in escape_characters.items():
+    print('Substitute', htmlesc, 'to', latexesc)
+    CV_tex_clean = CV_tex_clean.replace(htmlesc, latexesc)
+# Write the output to tex file
+file = open('files/CV_Nurfatima_Jandarova.tex', 'w')
+file.write(CV_tex_clean)
+file.close()
